e572fc1d219797cd76d05707ad716a5788629df2
xilemon <56141596+xilemon@users.noreply.github.com>
2022-09-19 13:54:00 +0800

    Update ydtjs01.js

--- ydtjs01.js
+++ ydtjs01.js
@@ -1,22 +1,20 @@
 let ydtjson = JSON.parse($response.body);
-ydtjson = {
-    "code": 0,
-    "result": {
-      
-            "resourceId": null,
-            "residualSecond": "0",
-            "rightsType": "SPEED_VIP_KM4",
-            "level": 0,
-            "description": null,
-            "beginTime": "2022-09-17",
-            "endTime": "2099-09-17",
-            "userBindDeviceList": [],
-            "deviceOverflow": "0",
-            "status": "2",
-            "overdueSecond": "0"
 
-    }
-    "msg": "ok"
+ydtjson = {
+  "code": 0,
+  "msg": "OK",
+  "result": {
+    "resourceld": "",
+    "residualSecond": "0",
+    "rightsType": "SPEED_VIP_KM1",
+    "level": "0",
+    "description": null,
+    "beginTime": "2022-09-17",
+    "endTime": "2099-09-17",
+    "userBindDeviceList": [],
+    "deviceOverflow": "0",
+    "status": "2",
+    "overdueSecond": "0"
+  }
 };
-$done({ body: JSON.stringify(ydtjson)
-});
+$done({ body: JSON.stringify(ydtjson) });
