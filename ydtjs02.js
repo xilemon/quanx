c4617dabcf2c82c030da031c38d788320d519fd8
xilemon <56141596+xilemon@users.noreply.github.com>
2022-09-19 10:22:34 +0800

    Create ydtjs02.js

---
+++ ydtjs02.js
@@ -0,0 +1,126 @@
+let ydtjson = JSON.parse($response.body);
+ydtjson = {
+    "code": 0,
+    "result": {
+        "SPEED_VIP_KM4": {
+            "resourceId": "",
+            "residualSecond": "0",
+            "rightsType": "SPEED_VIP_KM4",
+            "level": 0,
+            "description": null,
+            "beginTime": "2022-09-17",
+            "endTime": "2099-09-17",
+            "userBindDeviceList": [],
+            "deviceOverflow": "0",
+            "status": "2",
+            "overdueSecond": "0"
+        },
+        "RELIABLE_VIP_KM4": {
+            "resourceId": "",
+            "residualSecond": "0",
+            "rightsType": "RELIABLE_VIP_KM4",
+            "level": 0,
+            "description": null,
+            "beginTime": "2022-09-17",
+            "endTime": "2099-09-17",
+            "userBindDeviceList": [],
+            "deviceOverflow": "0",
+            "status": "2",
+            "overdueSecond": "0"
+        },
+        "RELIABLE_VIP_KM1": {
+            "resourceId": "",
+            "residualSecond": "0",
+            "rightsType": "RELIABLE_VIP_KM1",
+            "level": 0,
+            "description": null,
+            "beginTime": "2022-09-17",
+            "endTime": "2099-09-17",
+            "userBindDeviceList": [],
+            "deviceOverflow": "0",
+            "status": "2",
+            "overdueSecond": "0"
+        },
+        "SVIP_LIVECAMP": {
+            "resourceId": "",
+            "residualSecond": "0",
+            "rightsType": "SVIP_LIVECAMP",
+            "level": 0,
+            "description": null,
+            "beginTime": "2022-09-17",
+            "endTime": "2099-09-17",
+            "userBindDeviceList": [],
+            "deviceOverflow": "0",
+            "status": "2",
+            "overdueSecond": "0"
+        },
+        "VIP": {
+            "resourceId": "",
+            "residualSecond": "0",
+            "rightsType": "VIP",
+            "level": 0,
+            "description": null,
+            "beginTime": "2022-09-17",
+            "endTime": "2099-09-17",
+            "userBindDeviceList": [],
+            "deviceOverflow": "0",
+            "status": "2",
+            "overdueSecond": "56"
+        },
+        "SVIP": {
+            "resourceId": "",
+            "residualSecond": "0",
+            "rightsType": "SVIP",
+            "level": 0,
+            "description": null,
+            "beginTime": "2022-09-17",
+            "endTime": "2099-09-17",
+            "userBindDeviceList": [],
+            "deviceOverflow": "0",
+            "status": "2",
+            "overdueSecond": "0"
+        },
+        "SPEED_VIP_KM1": {
+            "resourceId": "",
+            "residualSecond": "0",
+            "rightsType": "SPEED_VIP_KM1",
+            "level": 0,
+            "description": null,
+            "beginTime": "2022-09-17",
+            "endTime": "2099-09-17",
+            "userBindDeviceList": [],
+            "deviceOverflow": "0",
+            "status": "2",
+            "overdueSecond": "0"
+        },
+        "SPEED_VIP_KM2": {
+            "resourceId": "",
+            "residualSecond": "0",
+            "rightsType": "SPEED_VIP_KM2",
+            "level": 0,
+            "description": null,
+            "beginTime": "2022-09-17",
+            "endTime": "2099-09-17",
+            "userBindDeviceList": [],
+            "deviceOverflow": "0",
+            "status": "2",
+            "overdueSecond": "0"
+        },
+        "SPEED_VIP_KM3": {
+            "resourceId": "",
+            "residualSecond": "0",
+            "rightsType": "SPEED_VIP_KM3",
+            "level": 0,
+            "description": null,
+            "beginTime": "2022-09-17",
+            "endTime": "2099-09-17",
+            "userBindDeviceList": [],
+            "deviceOverflow": "0",
+            "status": "2",
+            "overdueSecond": "0"
+        }
+    },
+    "msg": "ok"
+};
+$done({ body: JSON.stringify(obj)
+});
