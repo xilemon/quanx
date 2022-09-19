1ea5452a48b8c2d6c0cf29f2e6100e85bfe922ee
xilemon <56141596+xilemon@users.noreply.github.com>
2022-09-19 13:49:06 +0800

    Update ydtjs01.js

--- ydtjs01.js
+++ ydtjs01.js
@@ -2,7 +2,7 @@ let ydtjson = JSON.parse($response.body);
 ydtjson = {
     "code": 0,
     "result": {
-        "SPEED_VIP_KM4": {
+      
             "resourceId": null,
             "residualSecond": "0",
             "rightsType": "SPEED_VIP_KM4",
@@ -14,73 +14,8 @@ ydtjson = {
             "deviceOverflow": "0",
             "status": "2",
             "overdueSecond": "0"
-        },
-        "RELIABLE_VIP_KM4": {
-            "resourceId": null,
-            "residualSecond": "0",
-            "rightsType": "RELIABLE_VIP_KM4",
-            "level": 0,
-            "description": null,
-            "beginTime": "2022-09-17",
-            "endTime": "2099-09-17",
-            "userBindDeviceList": [],
-            "deviceOverflow": "0",
-            "status": "2",
-            "overdueSecond": "0"
-        },
-        "EXAM_SKILL_KM4": {
-            "resourceId": null,
-            "residualSecond": "0",
-            "rightsType": "EXAM_SKILL_KM4",
-            "level": 0,
-            "description": null,
-            "beginTime": "2022-09-17",
-            "endTime": "2099-09-17",
-            "userBindDeviceList": [],
-            "deviceOverflow": "0",
-            "status": "2",
-            "overdueSecond": "0"
-        },
-        "SPEED_VIP_KM1": {
-            "resourceId": null,
-            "residualSecond": "0",
-            "rightsType": "SPEED_VIP_KM1",
-            "level": 0,
-            "description": null,
-            "beginTime": "2022-09-17",
-            "endTime": "2099-09-17",
-            "userBindDeviceList": [],
-            "deviceOverflow": "0",
-            "status": "2",
-            "overdueSecond": "0"
-        },
-        "RELIABLE_VIP_KM1": {
-            "resourceId": null,
-            "residualSecond": "0",
-            "rightsType": "RELIABLE_VIP_KM1",
-            "level": 0,
-            "description": null,
-            "beginTime": "2022-09-17",
-            "endTime": "2099-09-17",
-            "userBindDeviceList": [],
-            "deviceOverflow": "0",
-            "status": "2",
-            "overdueSecond": "0"
-        },
-        "EXAM_SKILL_KM1": {
-            "resourceId": null,
-            "residualSecond": "0",
-            "rightsType": "EXAM_SKILL_KM1",
-            "level": 0,
-            "description": null,
-            "beginTime": "2022-09-17",
-            "endTime": "2099-09-17",
-            "userBindDeviceList": [],
-            "deviceOverflow": "0",
-            "status": "2",
-            "overdueSecond": "0"
-        }
-    },
+
+    }
     "msg": "ok"
 };
 $done({ body: JSON.stringify(ydtjson)
